54b290cb7e9a5f75014a41da276e123cd1f072bf
SangeeMega <sangeethameganathan16@gmail.com>
2019-11-21 12:26:40 +0530

    c1

--- app/src/main/java/com/example/myapp/AddCard.java
+++ app/src/main/java/com/example/myapp/AddCard.java
@@ -9,5 +9,6 @@ public class AddCard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
+        // sangeetha
     }
 }
