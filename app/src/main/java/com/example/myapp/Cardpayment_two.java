afa3cb731c96f713d906dd9801f24ba3df5d46e7
admin <pridanandini.amigatechnologies@outlook.com>
2019-12-13 15:35:59 +0530

    java file

---
+++ app/src/main/java/com/example/myapp/Cardpayment_two.java
@@ -0,0 +1,13 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Cardpayment_two extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardpayment_two);
+    }
+}
